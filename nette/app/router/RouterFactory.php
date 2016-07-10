95665a0cdb57b78a72b3155eb76aef7fddab9094
jrypacek <j.rypacek@gmail.com>
2016-07-10 18:05:41 +0200

    still need secured < 2.4

--- nette/app/router/RouterFactory.php
+++ nette/app/router/RouterFactory.php
@@ -16,7 +16,7 @@ class RouterFactory
 	public static function createRouter()
 	{
 		$router = new RouteList;
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Charts:default');
+		$router[] = new Route('<presenter>/<action>[/<id>]', 'Charts:default', Route::SECURED);
 		return $router;
 	}
 
