567287e742b44a6bb94b14cd57123eabb91a1b2d
jrypacek <j.rypacek@gmail.com>
2016-09-17 17:49:00 +0200

    Bower, nette & grido update

--- nette/app/presenters/SettingsPresenter.php
+++ nette/app/presenters/SettingsPresenter.php
@@ -20,12 +20,14 @@ class SettingsPresenter extends BasePresenter
 	protected function createComponentGrid($name)
 	{
 		$grid = new \Grido\Grid($this, $name);
+		$grid->setFilterRenderType(\Grido\Components\Filters\Filter::RENDER_INNER);
 		$grid->setModel($this->database->table('device')->where(['user_id' => $this->user->getId()]));
 	
 		$grid->setPrimaryKey('int_id_grido');
 		$grid->setEditableColumns();
 		
-		$grid->addColumnText('name', 'Název');
+		$grid->addColumnText('name', 'Název')
+				->setFilterText(); // dont't need, but can't hide 'SEARCH' button... :(
 		$grid->addColumnNumber('order', 'Pořadí');
 		$grid->addColumnText('color', 'Barva');
 	}
